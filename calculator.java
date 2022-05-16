d64203d523cf51049f322b7a32bc1f5415b698cd
Hitku <97933783+Hitoli@users.noreply.github.com>
2022-05-16 14:42:56 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String args[]){
+       int n=1;
+        while(n!=0){
+            System.out.println("please Enter the operator:");
+            Scanner in = new Scanner(System.in);
+            char operator = in.next().charAt(0);
+            if(operator=='x'){
+                break;
+            }
+            System.out.println("please enter two numbers:");
+
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int c=0;
+
+
+            switch (operator){
+                case'+'-> c=a+b;
+                case'-'-> c=a-b;
+                case'/'->c=a/b;
+                case'%'->c=a%b;
+                case'x'-> n=0;
+                default-> System.out.println("please Enter a valid operator");
+            }
+            System.out.println("ANS:"+c);
+
+        }
+
+
+
+
+
+    }
+}
